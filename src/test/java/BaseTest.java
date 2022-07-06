abd1cae5c482849932e756badfb911750f98bb2f
Avinash Kannan <87224683+Avinash-Kannan@users.noreply.github.com>
2022-07-06 16:47:55 +0530

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -23,3 +23,26 @@ public class BaseTest {
 
     }
 }
+// Test Activities  :
+
+// Sprint  Start
+
+// User story kick off - Both QA’s, BA’s, Dev
+
+// Create QA : Manual - Sub task 
+// {Description : Platform , OS Version, Device Model , Functionality}
+
+// QA Work -  TCs -> VSO  ( Assign priorities )
+
+// Create  DevBox sub task - QA / Both QA’s, BA’s, Dev
+
+// User Story - QA In Progress -> Manual Execution -> Bug sub tasks if any ( screenshots ) 
+
+// After QA complete (If Bugs are open) - Change story to Bug fixing
+
+// User Story ( QA and Bug fixing Done) - QA Sign Off
+
+// [ In Sprint /  n+1 Sprint ]
+// Create Regression / Smoke Automation Story - Link TCs  (Add Tags - Regression , Smoke)
+
+// End of Sprint 
